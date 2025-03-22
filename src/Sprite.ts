c6486251e730b888a9fbf22559abb3e661e7d78d
thembones79 <michal.szumnarski@gmail.com>
2025-03-22 12:22:13 +0100

    animations

--- src/Sprite.ts
+++ src/Sprite.ts
@@ -8,6 +8,7 @@ export interface ISpriteConfig {
   animations?: Record<string, MoveType>;
   currentAnimation?: string;
   currentAnimationFrame?: number;
+  animationFrameLimit?: number;
 }
 
 export class Sprite {
@@ -20,6 +21,8 @@ export class Sprite {
   animations: Record<string, MoveType>;
   currentAnimation: string;
   currentAnimationFrame: number;
+  animationFrameLimit: number;
+  animationFrameProgress: number;
 
   constructor(config: ISpriteConfig) {
     this.gameObject = config.gameObject;
@@ -40,20 +43,35 @@ export class Sprite {
 
     this.animations = config.animations || {
       idleDown: [[0, 0]],
+      "idle-down": [[0, 0]],
       walkDown: [
         [0, 0],
         [1, 0],
         [2, 0],
         [3, 0],
       ],
+      "walk-down": [
+        [1, 0],
+        [0, 0],
+        [3, 0],
+        [0, 0],
+      ],
     };
-    this.currentAnimation = config.currentAnimation || "idleDown";
+    // this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimation = "walk-down";
     this.currentAnimationFrame = 0;
+
+    this.animationFrameLimit = config.animationFrameLimit || 16;
+    this.animationFrameProgress = this.animationFrameLimit;
+  }
+
+  get frame() {
+    return this.animations[this.currentAnimation][this.currentAnimationFrame];
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    const x = this.gameObject.x  - 8;
-    const y = this.gameObject.y  - 18;
+    const x = this.gameObject.x - 8;
+    const y = this.gameObject.y - 18;
     this.isShadowLoaded && ctx.drawImage(this.shadow, x, y);
     this.isLoaded && ctx.drawImage(this.image, 0, 0, 32, 32, x, y, 32, 32);
   }
